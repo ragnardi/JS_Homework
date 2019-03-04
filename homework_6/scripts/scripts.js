6ee6050b7b46946918c2d0c1de4dbe27724ebd47
Igor Shyrochyn <rainwalkingman@gmail.com>
2019-03-04 06:45:32 +0200

    homework_6

---
+++ homework_6/scripts/scripts.js
@@ -0,0 +1,8 @@
+//open and close search field in header
+$('#search-caller').click(function() {
+    $('#top-searcher').toggle('slide');
+});
+
+$('.menu-button').click(function() {
+    $('.navigation').slideToggle(500);
+});

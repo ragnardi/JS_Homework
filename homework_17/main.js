eec0965727f5e5fa7107de6a2d8558fa32e8ea45
Igor Shyrochyn <rainwalkingman@gmail.com>
2019-04-08 08:28:09 +0300

    homework_17 fixed

--- homework_17/main.js
+++ homework_17/main.js
@@ -1,7 +1,8 @@
-var template = new RegExp(/^((8-?0)|(\+?375))-?((25)|(29)|(33)|(44))-?\d{3}-?\d{2}-?\d{2}$/);
+var template = new RegExp(/^((8-?0)|(\+?375-?))((25)|(29)|(33)|(44))-?\d{3}-?\d{2}-?\d{2}$/);
 
 template.test('80297452233');
 template.test('+375-29-333-44-55');
 template.test('375339996677');
 template.test('8-0447778899');
 template.test('+37529333-4455');
+template.test('8-0-447776655');  //false
